c3cd3747ddd15f1c8c81f348d53520ead5aee81e
emmanuel <mcdecoil@gmail.com>
2023-01-08 15:35:41 +0000

    indent fixed

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,20 +3,21 @@
 Lockbox task
 """
 
+
 def canUnlockAll(boxes):
-	"""
-	Determines if all the boxes can be opened or not
-	Returns:
-		True: all boxes can be opened
-		False: not all boxes can be opened
-	"""
-	n = len(boxes)
-	opened = [0]
-	for i in opened:
-		for j in boxes[i]:
-			if j not in opened:
-				if j < n:
-					opened.append(j)
-	if len(opened) == n:
-		return True
-	return False
+    """
+    Determines if all the boxes can be opened or not
+    Returns:
+        True: all boxes can be opened
+        False: not all boxes can be opened
+    """
+    n = len(boxes)
+    opened = [0]
+    for i in opened:
+        for j in boxes[i]:
+            if j not in opened:
+                if j < n:
+                    opened.append(j)
+    if len(opened) == n:
+        return True
+    return False

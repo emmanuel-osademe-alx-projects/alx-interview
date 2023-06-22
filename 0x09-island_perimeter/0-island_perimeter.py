61bcfaf9bb4ef43dd53371267b525acf907425f0
Emmanuel Osademe ALX Projects <mcdecoil@gmail.com>
2023-06-22 09:02:30 +0000

    task done

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""module docs for 0-island_perimeter.py"""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid"""
+    if not grid or not grid[0]:
+        return 0
+    rows = len(grid)
+    cols = len(grid[0])
+    perimeter = 0
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4  # Start with assuming all sides are part of the perimeter
+                # Check left neighbor
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2  # Deduct 2 for overlapping side
+                # Check top neighbor
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2  # Deduct 2 for overlapping side
+    return perimeter

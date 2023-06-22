2fa32847b7f63f52201286040fe410495f18ccc2
Emmanuel Osademe ALX Projects <mcdecoil@gmail.com>
2023-06-22 09:43:28 +0000

    update task

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""module docs for 0-prime_game.py"""
+
+
+def isWinner(x, nums):
+    """returns the winner of the game"""
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        primes = [True] * (n + 1)
+        primes[0] = primes[1] = False
+
+        # Sieve of Eratosthenes to find all prime numbers
+        for i in range(2, int(n ** 0.5) + 1):
+            if primes[i]:
+                for j in range(i * i, n + 1, i):
+                    primes[j] = False
+
+        # Check if Maria or Ben wins the current round
+        if primes.count(True) % 2 == 1:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
